a34920f4bcb1c1205d2e55369a536a498c65278e
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-12-19 17:39:12 +0100

    adminapi: Remove deprecated top-level functions

--- adminapi/__init__.py
+++ adminapi/__init__.py
@@ -2,16 +2,3 @@
 
 Copyright (c) 2018 InnoGames GmbH
 """
-
-from adminapi.request import Settings
-
-
-# XXX Deprecated
-def auth(auth_token=None):
-    if auth_token:
-        Settings.auth_token = auth_token
-
-
-# XXX Deprecated
-def set_timeout(timeout, what='api'):
-    Settings.timeout = timeout
